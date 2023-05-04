463a932925ca7e0def6e04e66038f593024dbaea
Teon Ooi <teon@protonmail.com>
2023-05-04 15:35:41 +0800

    Create initial commit.

---
+++ Lexitor/LexitorApp.swift
@@ -0,0 +1,17 @@
+//
+//  LexitorApp.swift
+//  Lexitor
+//
+//  Created by Teon Ooi on 04/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct LexitorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
